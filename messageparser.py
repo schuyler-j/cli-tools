492adada5780c532762a244ea65667fd6dfda8ab
gritz <gritz@DESKTOP>
2022-12-06 12:32:22 +1030

    added args

--- messageparser.py
+++ messageparser.py
@@ -2,91 +2,110 @@ import re
 import random
 import time
 import matplotlib.pyplot as plt
+import os
+import sys
 
-file = '2022-12-05-stream-msgs.txt'
-msg = open(file, 'r').read()
+#potentially auto run messagescraperloop.py
 
-chunks = msg.split(';')
 
-pattern = r'^display-name='
-count = 0
 
-usernames = []
 
-for chunk in chunks:
-    x = re.findall(pattern, chunk)
-    count += 1
-    if x:
-        username = chunks[count-1].split('=')[1]
-        usernames.append(username)
+def parser():
+    file = 'msgfull.txt'
 
-ncount = 0
-unique = []
-for name in usernames:
-    tempname = name
-    for n in usernames:
-        if n == tempname:
-            unique.append(n)
-            ncount += 1
-            break
+    #if file doesn't exist
+    try:
+        msg = open(file, 'r').read()
+
+    except Exception as e:
+        print("NO FILE", e)
+        sys.exit()
+    chunks = msg.split(';')
+
+    pattern = r'^display-name='
+    count = 0
+
+    usernames = []
+
+    for chunk in chunks:
+        x = re.findall(pattern, chunk)
+        count += 1
+        if x:
+            username = chunks[count-1].split('=')[1]
+            usernames.append(username)
+
     ncount = 0
+    unique = []
+    for name in usernames:
+        tempname = name
+        for n in usernames:
+            if n == tempname:
+                unique.append(n)
+                ncount += 1
+                break
+        ncount = 0
 
 
-userdict = {}
-for name in usernames:
-    tempname = name
+    userdict = {}
+    for name in usernames:
+        tempname = name
 
-    for n in usernames:
-        if n == tempname:
-            ncount += 1
-            userdict.update({tempname: ncount})
-    ncount = 0
+        for n in usernames:
+            if n == tempname:
+                ncount += 1
+                userdict.update({tempname: ncount})
+        ncount = 0
 
-msgtimes = []
-time_pattern = r'rm-received-ts'
-count = 0
-for chunk in chunks:
-    x = re.findall(time_pattern, chunk)
-    count += 1
-    if x:
-        times = chunks[count-1].split('=')[1]
-        times_c = int(times)/1000
-        msgtimes.append(times_c)
+    msgtimes = []
+    time_pattern = r'rm-received-ts'
+    count = 0
+    for chunk in chunks:
+        x = re.findall(time_pattern, chunk)
+        count += 1
+        if x:
+            times = chunks[count-1].split('=')[1]
+            times_c = int(times)/1000
+            msgtimes.append(times_c)
 
 
-#sort by most username occurences
-#print(sorted(userdict.items(), key=lambda x: x[1], reverse=True))
+    #sort by most username occurences
+    #print(sorted(userdict.items(), key=lambda x: x[1], reverse=True))
 
-#time conversion
-converted_times = []
-for t in msgtimes:
-    converted_time = time.ctime(t)
-    converted_times.append(converted_time)
+    #time conversion
+    converted_times = []
+    for t in msgtimes:
+        converted_time = time.ctime(t)
+        converted_times.append(converted_time)
 
-#temp plot
-y = []
-x = msgtimes[:11]
-for s in userdict:
-    y.append(userdict[s])
-plt.plot(x, y)
+    #temp plot
+    """ y = []
+    x = msgtimes[:11]
+    for s in userdict:
+        y.append(userdict[s])
+    plt.plot(x, y) """
 
-pattern = r'^user-type='
+    pattern = r'^user-type='
 
-place = 0
-msgs = []
-for chunk in chunks:
-    x = re.findall(pattern, chunk)
-    place += 1
-    if x:
-       """  message = chunks[count-1].split('=')[1] """
-       message = chunks[place-1].split(' ')
-       s = message[4: len(message)-1]
-       if s == []:
-        pass
-       else:
-        msgs.append(message[4: len(message)])
+    place = 0
+    msgs = []
+    for chunk in chunks:
+        x = re.findall(pattern, chunk)
+        place += 1
+        if x:
+            """  message = chunks[count-1].split('=')[1] """
+        message = chunks[place-1].split(' ')
+        s = message[4: len(message)-1]
+        if s == []:
+            pass
+        else:
+            msgs.append(message[4: len(message)])
+
+
+    return msgs, converted_times
 
 def printChats():
+    msgs = parser()[0]
+    converted_times = parser()[1]
     fmtdchats = []
     count = 0
     for m in msgs:
@@ -96,10 +115,11 @@ def printChats():
         fmtdchats.append(t[0])
         count += 1
 
-def emoteCounter(p, f, d):
+def emoteCounter(f, d):
+    msgs = parser()[0]
     ocount = 0
     tcount = 0
-    pattern = p
+    pattern = input('Enter emote: ')
     counter = 0
     div = d
     length = len(msgs)
@@ -120,12 +140,37 @@ def emoteCounter(p, f, d):
         prb = (tcount / len(msgs)) * 100
     print(f'{pattern} was said {tcount} times in the last {length} messages, which is {prb}% of the messages')
 
+#test cases
 '''
 emoteCounter(r'GIGACHAD', 0, 10)
 emoteCounter(r'GIGACHAD', 0, 10)
 emoteCounter(r'LULW', 0, 10)
 emoteCounter(r'Aware', 0, 10)
-'''      
-printChats()
 emoteCounter(r'Nerd', 0, 10)
+'''      
 
+
+
+
+
+args = sys.argv[1:]
+
+if not args:
+    print("No arguments given")
+    sys.exit()
+
+options = {'-p': 'print chat messages', '-e': 'print emote count', '-a': 'auto run scraper'}
+
+commands = {
+    '-a': lambda: os.system('python messagescraperloop.py'),
+    '-h': lambda: print(f'Usage: python messageparser.py {options}', options),
+    '-p': lambda: printChats(),
+    '-e': lambda: emoteCounter(0, 10)
+}
+
+# Iterate over the arguments
+for arg in args:
+    # Check if the argument is a valid option
+    if arg in commands:
+        # Call the function associated with the option
+        commands[arg]()
